2acce09abd2dce377c3d6f0cbe955e93c7cd4442
Gabriel <GABEHJ2001@GMAIL.COM>
2022-02-26 10:02:35 -0800

    Update functionsExamples.py

--- functionsExamples.py
+++ functionsExamples.py
@@ -1,1 +1,12 @@
 
+mySentence = 'loves the color'
+colorList = ['Red','Orange','Yellow','Green','Blue','Purple','Pink']
+
+def colorFunction(name):
+    for i in colorList:
+        print('{} {} {}'.format(name, mySentence, i))
+        return msg
+
+colorFunction('Gabe')
+
+

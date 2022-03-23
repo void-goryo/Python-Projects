30d3ee223716421be8e617e3aef320c03faee3f0
Gabriel <GABEHJ2001@GMAIL.COM>
2022-03-23 09:30:34 -0700

    Create abstractionChallenge.py

---
+++ abstractionChallenge.py
@@ -0,0 +1,20 @@
+
+
+from abc import ABC, abstractmethod
+class car(ABC):
+    def paySlip(self, amount):
+        print('Your purchase amount: ',amount)
+    #this function is telling us to pass in an argument, but it doesn't tell you
+    #how or what kind of data it will be
+    @abstractmethod
+    def payment(self, amount):
+        pass
+
+class debitCardPayment(car):
+    def payment(self, amount):
+        print('Your purchase amount of {} exceeded your 100$ limit'.format(amount))
+
+if __name__ == '__main__':
+    obj = debitCardPayment()
+    obj.paySlip('400')
+    obj.payment('400')
